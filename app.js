7cd95a6ca5a9a106d5bee6d9cdbecfd67c39abc7
Antariksh Mahajan <antarikshmahajan@gmail.com>
2020-05-09 21:53:22 +0800

    fix: Edit app.js to match example

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
-app.use(express.static("dist"))
+// app.use(express.static("dist"))
 
-app.listen(3000, () => console.log("Listening on port 3000.")) 
+// app.listen(3000, () => console.log("Listening on port 3000.")) 
+
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+	if (req.method === 'GET') {
+		res.writeHead(200)
+		res.write(fs.readFileSync('dist/index.html'))
+		res.end()
+	}
+})
+
+server.listen(3000)
+console.log(`Server running at port 3000.`)
